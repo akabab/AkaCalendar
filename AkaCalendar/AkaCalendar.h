dba3a29937890f7164d1555cde1cef247d01a063
Yoann Cribier <yoann.cribier@openjet.com>
2016-05-25 10:58:37 +0200

    Initial Commit

---
+++ AkaCalendar/AkaCalendar.h
@@ -0,0 +1,19 @@
+//
+//  AkaCalendar.h
+//  AkaCalendar
+//
+//  Created by Yoann Cribier on 25/05/2016.
+//  Copyright © 2016 akabab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AkaCalendar.
+FOUNDATION_EXPORT double AkaCalendarVersionNumber;
+
+//! Project version string for AkaCalendar.
+FOUNDATION_EXPORT const unsigned char AkaCalendarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AkaCalendar/PublicHeader.h>
+
+
